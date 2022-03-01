9fbaba5f443db45f2b5fd3b1ebfe1bdfca27a4f4
Wellington Paim <88003397+wellingtonpaim@users.noreply.github.com>
2022-02-28 22:36:12 -0300

    Adicionado o projeto.
    Projeto até o módulo 4.

---
+++ algafood-api/src/main/java/com/algaworks/algafood/jpa/InclusaoRestauranteMain.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.jpa;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+public class InclusaoRestauranteMain {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		RestauranteRepository restauranteRepository = applicationContext.getBean(RestauranteRepository.class);
+		
+		Restaurante restaurante1 = new Restaurante();
+		restaurante1.setNome("AlgaLicia");
+				
+		restaurante1 = restauranteRepository.salvar(restaurante1);
+				
+		System.out.printf("%d - %s\n", restaurante1.getId(), restaurante1.getNome());
+		
+	}
+}
